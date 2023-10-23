ad89f2dafbaaa8964b81e1272d1bfba4b1707e9e
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-10-23 10:39:21 +0200

    Commit 1

--- app/src/main/java/com/example/timetablemanager/MainActivity.java
+++ app/src/main/java/com/example/timetablemanager/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.timetablemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView date;
+    private RelativeLayout rlTop;
+    private ImageView ivLeft, ivRight, ivPlus;
+    private ListView lvTop;
+    private LinearLayout llPlus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUI();
+    }
+
+    private void setUI() {
+        date = findViewById(R.id.date);
+        rlTop = findViewById(R.id.rlTop);
+        ivLeft = findViewById(R.id.ivLeft);
+        ivRight = findViewById(R.id.ivRight);
+        ivPlus = findViewById(R.id.ivPlus);
+        lvTop = findViewById(R.id.lvTop);
+        llPlus = findViewById(R.id.llPlus);
     }
 }
