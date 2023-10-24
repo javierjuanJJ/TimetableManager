ad97e1bdb561696af7a96184470b236712be5f57
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-10-24 11:00:15 +0200

    Commit 2.2

---
+++ app/src/main/java/com/example/timetablemanager/Task.java
@@ -0,0 +1,135 @@
+package com.example.timetablemanager;
+
+import android.content.Context;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.content.res.ResourcesCompat;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+public class Task implements Comparable<Task> {
+   private int id;
+   private String task, color;
+   private LocalTime from, to;
+   private DateTimeFormatter formatter;
+
+   @RequiresApi(api = Build.VERSION_CODES.O)
+   public Task(){
+      Calendar calendar = Calendar.getInstance();
+      from = getCalendarFrom(calendar);
+      to = getCalendarFrom(calendar);
+
+   }
+
+   @RequiresApi(api = Build.VERSION_CODES.O)
+   private static LocalTime getCalendarFrom(Calendar calendar) {
+      return LocalTime.of(calendar.get(Calendar.HOUR), calendar.get(Calendar.MINUTE));
+   }
+
+   public Task(int id, String task, String color, LocalTime from, LocalTime to, DateTimeFormatter formatter) {
+      this.id = id;
+      this.task = task;
+      this.color = color;
+      this.from = from;
+      this.to = to;
+      this.formatter = formatter;
+   }
+
+   public int getId() {
+      return id;
+   }
+
+   public void setId(int id) {
+      this.id = id;
+   }
+
+   public String getTask() {
+      return task;
+   }
+
+   public void setTask(String task) {
+      this.task = task;
+   }
+
+   public String getColor() {
+      return color;
+   }
+
+   public void setColor(String color) {
+      this.color = color;
+   }
+
+   @RequiresApi(api = Build.VERSION_CODES.O)
+   public String getFromToString() {
+      return from.format(formatter);
+   }
+
+   @RequiresApi(api = Build.VERSION_CODES.O)
+   public String getToString() {
+      return to.format(formatter);
+   }
+
+   @RequiresApi(api = Build.VERSION_CODES.O)
+   public void setFrom(String from) {
+      this.from = getParseDate(from);
+   }
+
+   @RequiresApi(api = Build.VERSION_CODES.O)
+   private LocalTime getParseDate(String from) {
+      return LocalTime.parse(from, formatter);
+   }
+
+
+
+   @RequiresApi(api = Build.VERSION_CODES.O)
+   public void setTo(String to) {
+      this.from = getParseDate(to);
+   }
+
+   public int getColorId(Context context){
+      int color = 0;
+      switch (getColor().toLowerCase()){
+         case "blue":
+            color = ResourcesCompat.getColor(context.getResources(), R.color.blue, null);
+            break;
+         case "rose":
+            color = ResourcesCompat.getColor(context.getResources(), R.color.rose, null);
+            break;
+         case "green":
+            color = ResourcesCompat.getColor(context.getResources(), R.color.green, null);
+            break;
+         case "red":
+            color = ResourcesCompat.getColor(context.getResources(), R.color.red, null);
+            break;
+         case "orange":
+            color = ResourcesCompat.getColor(context.getResources(), R.color.orange, null);
+            break;
+         case "grey":
+            color = ResourcesCompat.getColor(context.getResources(), R.color.grey, null);
+            break;
+         case "purple":
+            color = ResourcesCompat.getColor(context.getResources(), R.color.purple, null);
+            break;
+         case "yellow":
+            color = ResourcesCompat.getColor(context.getResources(), R.color.yellow, null);
+            break;
+         default:
+            color = ResourcesCompat.getColor(context.getResources(), R.color.black, null);
+            break;
+      }
+      return color;
+   }
+
+   public LocalTime getFrom() {
+      return from;
+   }
+
+   @RequiresApi(api = Build.VERSION_CODES.O)
+   @Override
+   public int compareTo(Task task) {
+      return from.compareTo(task.getFrom());
+   }
+}
--- app/src/main/java/com/example/timetablemanager/TaskEditor.java
+++ app/src/main/java/com/example/timetablemanager/TaskEditor.java
@@ -3,12 +3,41 @@ package com.example.timetablemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class TaskEditor extends AppCompatActivity {
+
+
+    private EditText etTask;
+    private TextView btnDelete, tvFrom, tvClickHere1, tvColor, tvClickHere2, tvColorSpinner;
+    private Spinner sColor;
+
+    private Button tvSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_editor);
+        setUI();
+    }
+
+    private void setUI() {
+        etTask = findViewById(R.id.etTask);
+
+        tvFrom = findViewById(R.id.tvFrom);
+
+        tvClickHere1 = findViewById(R.id.tvClickHere1);
+        tvClickHere2 = findViewById(R.id.tvClickHere2);
+
+        tvColor = findViewById(R.id.tvColor);
+        tvColorSpinner = findViewById(R.id.tvColorSpinner);
+
+        sColor = findViewById(R.id.sColor);
+
+        btnDelete = findViewById(R.id.tvDelete);
+        tvSubmit = findViewById(R.id.tvSubmit);
     }
 }
